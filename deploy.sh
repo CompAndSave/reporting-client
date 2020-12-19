6953f876e34e34797d55520b88c7964b1ad598ef
Andrew Y <andrew@compandsave.com>
2020-12-18 19:56:36 -0800

    add dotenv-plugin

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@
 ls -al
 ls -al $CODEBUILD_SRC_DIR/target/$env
 npm install -g serverless
+npm install -D serverless-dotenv-plugin
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v
